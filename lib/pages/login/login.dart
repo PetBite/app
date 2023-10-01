6aee9676a967303635bfac5f302e8a330cb120de
Wenhao Qiu <wenhaoq@hawaii.edu>
2023-09-30 21:22:38 -1000

    Login page complete

---
+++ lib/pages/login/login.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  static const routeName = '/';
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        children: [
+          const SizedBox(height: 50),
+          Image.asset('assets/images/loginpic.png', width: 150, height: 150),
+          const Text(
+            'Login',
+            style: TextStyle(fontSize: 30),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 50),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Email',
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                Checkbox(
+                  value: true,
+                  onChanged: (value) {},
+                ),
+                const Text('Remember me'),
+                const Spacer(),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Forgot Password?'),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 10),
+          Align(
+            alignment: Alignment.center,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/home');
+              },
+              style: ElevatedButton.styleFrom(
+                shape: const StadiumBorder(),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 130, vertical: 16),
+              ),
+              child: const Text(
+                'Login',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Don\'t have an account?'),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/register');
+                },
+                child: const Text('Register'),
+              ),
+            ],
+          ),
+          const SizedBox(height: 50),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  height: 1,
+                  color: Colors.grey,
+                ),
+              ),
+              const Text('Or login with'),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  height: 1,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MaterialButton(
+                onPressed: () {},
+                color: Colors.blue,
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(10),
+                shape: const CircleBorder(),
+                child: const Icon(
+                  Icons.facebook,
+                  size: 35,
+                ),
+              ),
+              MaterialButton(
+                onPressed: () {},
+                color: Colors.blue,
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(10),
+                shape: const CircleBorder(),
+                child: const Icon(
+                  Icons.apple,
+                  size: 35,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ));
+  }
+}
