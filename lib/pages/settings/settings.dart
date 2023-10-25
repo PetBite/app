4756a1d9ccb90f565d7007ba6cde057fbb5c6db9
Wenhao Qiu <wenhaoq@hawaii.edu>
2023-10-24 22:29:40 -1000

    Update settings.dart

--- lib/pages/settings/settings.dart
+++ lib/pages/settings/settings.dart
@@ -5,7 +5,7 @@ import 'package:app/data_model/user_db.dart';
 class Settings extends StatelessWidget {
   const Settings({super.key});
 
-  static const routeName = '/feeding_schedule';
+  static const routeName = '/profile';
 
   @override
   Widget build(BuildContext context) {
@@ -16,32 +16,36 @@ class Settings extends StatelessWidget {
         automaticallyImplyLeading: false,
       ),
       body: SafeArea(
-          child: ListView(children: const <Widget>[
-        ListTile(
+          child: ListView(children: <Widget>[
+        const ListTile(
             leading: Icon(Icons.manage_accounts, size: 35),
             title: Text('Account'),
             trailing: Icon(Icons.arrow_forward_ios)),
         ListTile(
-            leading: Icon(Icons.circle_notifications, size: 35),
-            title: Text('Notifications'),
-            trailing: Icon(Icons.arrow_forward_ios)),
-        ListTile(
+          leading: const Icon(Icons.settings, size: 35),
+          title: const Text('Settings'),
+          trailing: const Icon(Icons.arrow_forward_ios),
+          onTap: () {
+            Navigator.pushNamed(context, '/settings');
+          },
+        ),
+        const ListTile(
             leading: Icon(Icons.access_time, size: 36),
             title: Text('Time Settings'),
             trailing: Icon(Icons.arrow_forward_ios)),
-        ListTile(
+        const ListTile(
             leading: Icon(Icons.language, size: 35),
             title: Text('Language'),
             trailing: Icon(Icons.arrow_forward_ios)),
-        ListTile(
+        const ListTile(
             leading: Icon(Icons.location_on, size: 35),
             title: Text('Location'),
             trailing: Icon(Icons.arrow_forward_ios)),
-        ListTile(
+        const ListTile(
             leading: Icon(Icons.info_outline, size: 35),
             title: Text('App Information'),
             trailing: Icon(Icons.arrow_forward_ios)),
-        ListTile(
+        const ListTile(
             leading: Icon(Icons.help, size: 35),
             title: Text('Help'),
             trailing: Icon(Icons.arrow_forward_ios)),
