011d9d2ac5585d5e08a2b2ff284c2d91d0ec577c
Wenhao Qiu <wenhaoq@hawaii.edu>
2023-10-26 19:29:59 -1000

    Update edit_feeding_schedule.dart

--- lib/pages/feeding_schedule/edit_feeding_schedule.dart
+++ lib/pages/feeding_schedule/edit_feeding_schedule.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../data_model/feeding_schedule_db.dart';
+
 class EditFeedingSchedule extends ConsumerStatefulWidget {
   const EditFeedingSchedule({super.key});
 
@@ -14,6 +16,9 @@ class EditFeedingSchedule extends ConsumerStatefulWidget {
 class _EditFeedingScheduleState extends ConsumerState<EditFeedingSchedule> {
   @override
   Widget build(BuildContext context) {
+    final feedingScheduleDB = ref.watch(feedingScheduleDBProvider);
+    final List<FeedingScheduleData> feedingSchedules =
+        feedingScheduleDB.getAllFeedingSchedules();
     return Scaffold(
       appBar: AppBar(
         title: const Text("Edit Feeding Schedule"),
@@ -28,7 +33,28 @@ class _EditFeedingScheduleState extends ConsumerState<EditFeedingSchedule> {
       ),
       body: SafeArea(
           child: ListView(
-        children: [],
+        children: feedingSchedules
+            .map((schedule) => ListTile(
+                  title: Text(schedule.day),
+                  subtitle: const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Meal Name'),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Time'),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Food Type'),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Quantity'),
+                      ),
+                    ],
+                  ),
+                ))
+            .toList(),
       )),
     );
   }
