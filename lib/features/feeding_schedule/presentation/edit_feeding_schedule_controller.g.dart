941d191dd18d89c1b617b87edc71df2ee6e9095a
Wenhao Qiu <wenhaoq@hawaii.edu>
2023-11-17 20:17:48 -1000

    WIP

---
+++ lib/features/feeding_schedule/presentation/edit_feeding_schedule_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'edit_feeding_schedule_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$editFeedingScheduleControllerHash() =>
+    r'71038a3db03407b6529c6a6334935bb95fb6deea';
+
+/// See also [EditFeedingScheduleController].
+@ProviderFor(EditFeedingScheduleController)
+final editFeedingScheduleControllerProvider = AutoDisposeAsyncNotifierProvider<
+    EditFeedingScheduleController, void>.internal(
+  EditFeedingScheduleController.new,
+  name: r'editFeedingScheduleControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$editFeedingScheduleControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$EditFeedingScheduleController = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
